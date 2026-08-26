05e1ecb7352e0e2dd32878d8dcfef3efa2ed9a70
Bruno <brunomoisespalomino@gmail.com>
2026-08-26 12:28:35 -0500

    new

--- 02_insert_registros.sql
+++ 02_insert_registros.sql
@@ -1,7 +1,11 @@
 USE [COSTO_LABOR];
 
 INSERT INTO general.TG_CONFIGURACION(txtCodigoConfiguracion,numParametro,txtParametro,numMinimo,numMaximo,txtDescripcion,flgEstado) 
-VALUES('TIEMPO_INACTIVIDAD',30,'',0,0,'Tiempo de inactividad en minutos',1);
+VALUES('TIEMPO_INACTIVIDAD',30,'',0,0,'Tiempo de inactividad en minutos',1);
+INSERT INTO general.TG_CONFIGURACION(txtCodigoConfiguracion,numParametro,txtParametro,numMinimo,numMaximo,txtDescripcion,flgEstado) 
+VALUES('MAX_CANT_INTENTO_FALLIDO',5,'',0,25,'Cantidad de intentos fallidos antes del bloqueo',1);
+INSERT INTO general.TG_CONFIGURACION(txtCodigoConfiguracion,numParametro,txtParametro,numMinimo,numMaximo,txtDescripcion,flgEstado) 
+VALUES('TIEMPO_BLOQUEO_INICIO_SESION',15,'',0,30,'Tiempo de bloqueo de inicio de sesion en minutos',1);
 --
 INSERT INTO registro.TG_TIPO_DATO(codTipoDato,nomTipodato,flgEstado,fecCreacion,txtUsuarioCreacion) 
 VALUES('TEXTO','TEXTO',1,GETDATE(),'SYSTEM_COSTOLABOR');
